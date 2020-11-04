1b8e17d1844654791f1290d1a89fea4792ef2306
Gurban Azimli <Gurban.Azimli@azercell.com>
2020-11-05 01:03:33 +0400

    Item 2: Consider Builders over telecoping constructors

---
+++ src/com/company/item2/abstractBuilders/Demo.java
@@ -0,0 +1,15 @@
+package com.company.item2.abstractBuilders;
+
+public class Demo {
+    public static void main(String[] args) {
+        NyPizza nyPizza = new NyPizza.Builder(NyPizza.Size.SMALL)
+                .addTopping(Pizza.Topping.SAUSAGE)
+                .addTopping(Pizza.Topping.ONION)
+                .build();
+
+        Calzone calzone = new Calzone.Builder()
+                .addTopping(Pizza.Topping.HAM)
+                .sauceInside()
+                .build();
+    }
+}
